2298aad3c865262776dd66e7b751afd7f046f725
TC <hello@kode.email>
2017-10-09 04:05:14 -0700

    check if distribution group address exists and update LDIF template

--- groups/add_ldap_mail_group.sh
+++ groups/add_ldap_mail_group.sh
@@ -36,52 +36,56 @@ ldapworkdir="/some/path/to/ldap/scripts/mail/groups"
 ## Gather Group and Domain names
 # Group Name
 while true; do
-    echo -ne "Email Group Name$blue:$reset "
-    read mailgroup
-    mailgroup=$(echo "$mailgroup" | tr "[:upper:]" "[:lower:]")
+	echo -ne "Email Group Name$blue:$reset "
+	read mailgroup
+#mailgroup=$(echo "$first.$last" | tr "[:upper:]" "[:lower:]")
+	mailgroup=$(echo "$mailgroup" | tr "[:upper:]" "[:lower:]")
 
-    if [[ -z "$mailgroup" ]]; then
-        echo
-        printf "%s\n" "The Group Name CAN NOT be blank"
-        echo
-        continue #Skips the rest of the loop and starts again from the top.
-    fi
+	if [[ -z "$mailgroup" ]]; then
+		echo
+		printf "%s\n" "The Group Name CAN NOT be blank"
+		echo
+		continue #Skips the rest of the loop and starts again from the top.
+	fi
 
-     $(command -v ldapsearch) -xLLL -b "$mailgroupdn" | grep "cn=$mailgroup"
-     if [[ $? -eq 0 ]]; then
-       echo
-       printf "%s\n" "$mailgroup exists in LDAP"
-       echo
-       continue #Skips the rest of the loop and starts again from the top.
-     fi
-
-    #If execution reaches this point, both above checks have been passed
-    break #exit while loop, since we've got a valid group
-  done
+		#If execution reaches this point, both above checks have been passed
+		break #exit while loop, since we've got a valid group
+	done
 
 # Mail domain menu
-    echo
-    echo -e "$blue==================$reset"
-    echo -e "$white MAIL DOMAIN MENU$reset"
-    echo -e "$blue==================$reset"
-    array=$(printf "%s\n" "${domainlist[@]}")
-    echo "$array"
-    echo
+		echo
+		echo -e "$blue==================$reset"
+		echo -e "$white MAIL DOMAIN MENU$reset"
+		echo -e "$blue==================$reset"
+		array=$(printf "%s\n" "${domainlist[@]}")
+		echo "$array"
+		echo
   
-    # Get domain
+# Get domain
+while true; do
 	echo -ne "Enter the domain to use for the mail group$blue:$reset "
 	read domain
 	domain=$(echo "$domain" | tr "[:upper:]" "[:lower:]")
-	while [[ -z "$domain" ]]; do
-        echo
-        printf "%s\n" "The Domain Name CAN NOT be blank"
-        echo
-        echo -ne "Enter the domain to use for the mail group$blue:$reset "
-        read domain 
-	domain=$(echo "$domain" | tr "[:upper:]" "[:lower:]")
-	done
 
-        array=$(printf "%s\n" "${domainlist[@]}")
+	if [[ -z "$domain" ]]; then
+		echo
+		printf "%s\n" "The Domain Name CAN NOT be blank"
+		echo
+		continue
+	fi
+
+	$(command -v ldapsearch) -xLLL -b "$mailgroupdn" | grep "$mailgroup@$domain"
+	if [[ $? -eq 0 ]]; then
+		echo
+		printf "%s\n" "$mailgroup@$domain exists in LDAP"
+		echo
+		continue #Skips the rest of the loop and starts again from the top.
+	fi
+
+	break
+done
+
+	array=$(printf "%s\n" "${domainlist[@]}")
       
   	case $domain in
         "${domainlist[0]}")
@@ -104,7 +108,7 @@ while true; do
 esac
 
 # Set description to distribution group's name and mail domain
-description="$mailgroup email distribution group"
+description="$mailgroup@$domain distribution group"
 
 # First create LDAP working dir if it does not exist, then temporarily save basic info to create mailgroup.ldif
 if [[ ! -d "$ldapworkdir" ]];
